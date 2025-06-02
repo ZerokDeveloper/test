bf2d90edf9561236ab08dd92b1c08a6b6dbc8241
ZerokDev <63406899+ZerokDeveloper@users.noreply.github.com>
2025-06-02 22:44:50 +0200

    Update sprite_test.gd

--- sprite_test.gd
+++ sprite_test.gd
@@ -3,9 +3,4 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hola Mundo")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
