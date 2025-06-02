3cddda13e272de197538ae97f807e1ef084e7e6f
ZerokDev <63406899+ZerokDeveloper@users.noreply.github.com>
2025-06-02 22:55:31 +0200

    Update sprite_test.gd

--- sprite_test.gd
+++ sprite_test.gd
@@ -2,5 +2,10 @@ extends Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+func _ready():
+	#position.x = 500
+	#position.y = 250
+	position =Vector2(500, 250)
+	#scale.x = 2
+	#scale.y = 0.5
+	scale = Vector2(2, 0.5)
